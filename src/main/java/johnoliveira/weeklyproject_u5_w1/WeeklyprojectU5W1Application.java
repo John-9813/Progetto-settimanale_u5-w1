9247e1bd0e59c6d5535f0eef7e9ebca1da6f8dc2
John-9813 <john.oliver98.br@gmail.com>
2024-10-25 12:04:04 +0200

    start project

---
+++ src/main/java/johnoliveira/weeklyproject_u5_w1/WeeklyprojectU5W1Application.java
@@ -0,0 +1,13 @@
+package johnoliveira.weeklyproject_u5_w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyprojectU5W1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyprojectU5W1Application.class, args);
+	}
+
+}
---
+++ src/test/java/johnoliveira/weeklyproject_u5_w1/WeeklyprojectU5W1ApplicationTests.java
@@ -0,0 +1,13 @@
+package johnoliveira.weeklyproject_u5_w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyprojectU5W1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
